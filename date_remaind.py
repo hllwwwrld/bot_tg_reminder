2cbe60364c99bcf4f867b716fc01b03ed0405aca
hllwwwrld <arkoah072@gmail.com>
2023-05-16 20:24:07 +0500

    fixed hours remind hours (freezed time by first executing sql on sql-execute step)

--- date_remaind.py
+++ date_remaind.py
@@ -14,10 +14,9 @@ class ScheduleInfinite:
 
     def start_schedule():
         schedule.every(1).day.at('00:00').do(reminds.remind_dates)
-        schedule.every(1).hour.at(':00').do(reminds.remind_hours)
+        schedule.every(1).minute.at(':00').do(reminds.remind_hours)
 
         print(schedule.get_jobs())
-
         for job in schedule.get_jobs():
             print(job)
 
--- sql_db.py
+++ sql_db.py
@@ -1,10 +1,13 @@
+import datetime
+
 from db_conn import DbConnection
+from typing import Union
 
 
 class SqlRequests(DbConnection):
     # если fetch=True - запрос на выборку, нужно что-то вернуть, иначе запрос на изменение
     # fetchone - если надо вернуть одну строку
-    def execute_sql(self, sql: str, select: bool = True, fetchone: bool = False) -> list[tuple] | tuple:
+    def execute_sql(self, sql: str, select: bool = True, fetchone: bool = False) -> Union[tuple, list[tuple]]:
         try:
             self.database.execute(sql)
             if select:
@@ -99,7 +102,7 @@ class SqlRequests(DbConnection):
         where u.user_id = '{user_id}'
         {nearest}
         {month}
-        order by date_part('month', d.date), date_part('day', d.date)
+        order by date_part('month', d.date), date_part('day', d.date), date_part('minute', d.date)
         '''
         if nearest:
             res = self.execute_sql(sql_request, fetchone=True)
@@ -119,15 +122,17 @@ class SqlRequests(DbConnection):
     def get_remind_date(self, user_id: str, user_days_remind_to: int = 3) -> list[tuple[str | int]]:
         sql_request = f"""
         select d."name", d."date", u.user_id, 
-    {user_days_remind_to} - (date_part('day', age(d.date::date - interval '{user_days_remind_to} days'))) as "days_until_date",
-    date_part('month', age(d.date - interval '{user_days_remind_to} days')) as "month_until_date"
-    from dates d
-    join "user" u 
-    on d.user_id = u.tab_id 
-    where date_part('month', age(d.date::date - interval '{user_days_remind_to} days')) = 0
-    and {user_days_remind_to} - (date_part('day', age(d.date::date - interval '{user_days_remind_to} days'))) between 0 and {user_days_remind_to}
-    and u.user_id = '{user_id}'
-    order by 4
+        {user_days_remind_to} - (date_part('day', age(d.date::date - interval '{user_days_remind_to} days')))\
+        as "days_until_date",
+        date_part('month', age(d.date - interval '{user_days_remind_to} days')) as "month_until_date"
+        from dates d
+        join "user" u 
+        on d.user_id = u.tab_id 
+        where date_part('month', age(d.date::date - interval '{user_days_remind_to} days')) = 0
+        and {user_days_remind_to} - (date_part('day', age(d.date::date - interval '{user_days_remind_to} days')))\
+        between 0 and {user_days_remind_to}
+        and u.user_id = '{user_id}'
+        order by 4
         """
         res = self.execute_sql(sql_request)
         return res
@@ -135,8 +140,8 @@ class SqlRequests(DbConnection):
     def get_remind_hours(self, user_id: str) -> list[tuple[str | int]]:
         sql_request = f'''
         select d."name", d."date", u.user_id, 
-        date_part('hour', d."date"::time - current_time::time) as hours_until,
-        date_part('minute', d."date"::time - current_time::time) as minutes_until,
+        date_part('hour', d."date"::time - '{str(datetime.datetime.now().time())}'::time) as hours_until,
+        date_part('minute', d."date"::time - '{str(datetime.datetime.now().time())}'::time) as minutes_until,
         3 - (date_part('day', age(d.date::date - interval '3 days'))) as "days_until_date"
         --date_part('month', age(d.date::date - interval '3 days')) as "month_until"
         from dates d
@@ -144,11 +149,11 @@ class SqlRequests(DbConnection):
         on d.user_id = u.tab_id 
         where 
         case 
-            when date_part('hour', d."date"::time - current_time::time) = 0
-            then date_part('minute', d."date"::time - current_time::time) >= 0
-            when date_part('hour', d."date"::time - current_time::time) = 3
-            then date_part('minute', d."date"::time - current_time::time) = 0
-            else date_part('hour', d."date"::time - current_time::time) between 1 and 2
+            when date_part('hour', d."date"::time - '{str(datetime.datetime.now().time())}'::time) = 0
+            then date_part('minute', d."date"::time - '{str(datetime.datetime.now().time())}'::time) >= 0
+            when date_part('hour', d."date"::time - '{str(datetime.datetime.now().time())}'::time) = 3
+            then date_part('minute', d."date"::time - '{str(datetime.datetime.now().time())}'::time) = 0
+            else date_part('hour', d."date"::time - '{str(datetime.datetime.now().time())}'::time) between 1 and 2
         end
         and
         case
@@ -161,7 +166,9 @@ class SqlRequests(DbConnection):
         and u.user_id = '{user_id}'
         order by 4
         '''
+
         res = self.execute_sql(sql_request)
+        print(self.execute_sql(f"select '{str(datetime.datetime.now().time())}'::time"))
         return res
 
     def delete_date(self, user_id: str, name: str) -> None:
